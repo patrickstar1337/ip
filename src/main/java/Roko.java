70b119ed7d32f8c03057c7f3d31f54ae2e5dc299
jun hui <patrickstarjh@gmail.com>
2026-01-23 13:43:33 +0800

    added abilty to store text and display back

--- src/main/java/Roko.java
+++ src/main/java/Roko.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Roko {
     public static void main(String[] args) {
+        List<String> localdb = new ArrayList<>();
+
         System.out.println("Hello I'm ROKO");
         System.out.println("What can I do for you?");
 
@@ -12,8 +14,15 @@ public class Roko {
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println(formatLine + "Bye. Hope to see you again!" + formatLine);
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                int count = 1;
+                for (String i : localdb) {
+                    System.out.println(count + ". " + i);
+                    count++;
+                }
             } else {
-                System.out.println(formatLine + userInput + formatLine);
+                System.out.println(formatLine + "added: " + userInput + formatLine);
+                localdb.add(userInput);
             }
         }
     }
