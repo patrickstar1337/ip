059159b038681e149692df3f0048301a3783ea98
jun hui <patrickstarjh@gmail.com>
2026-01-23 14:13:04 +0800

    added ability to mark as done or undone

--- src/main/java/Roko.java
+++ src/main/java/Roko.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Roko {
     public static void main(String[] args) {
-        List<String> localdb = new ArrayList<>();
+        List<Task> localdb = new ArrayList<>();
 
         System.out.println("Hello I'm ROKO");
         System.out.println("What can I do for you?");
@@ -11,18 +11,36 @@ public class Roko {
         String formatLine = "\n------------------------------\n";
         while (true) {
             String userInput = scan.nextLine();
+            if (userInput.toLowerCase().contains("mark")) {
+                String[] temp = userInput.split(" ");
+                String command = temp[0];
+                int taskNo = Integer.parseInt(temp[1]) - 1;
+                if (command.equals("mark")) {
+                    localdb.get(taskNo).isDone = true;
+                    System.out.println("I've marked this as done:");
+                    System.out.println("[" + localdb.get(taskNo).getStatusIcon() + "] " +
+                            localdb.get(taskNo).description);
+                } else {
+                    localdb.get(taskNo).isDone = false;
+                    System.out.println("I've marked this as NOT done yet:");
+                    System.out.println("[" + localdb.get(taskNo).getStatusIcon() + "] " +
+                            localdb.get(taskNo).description);
+                }
+                continue;
+            }
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println(formatLine + "Bye. Hope to see you again!" + formatLine);
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
                 int count = 1;
-                for (String i : localdb) {
-                    System.out.println(count + ". " + i);
+                for (Task task : localdb) {
+                    System.out.println(count + ".[" + task.getStatusIcon() + "] " + task.description);
                     count++;
                 }
             } else {
                 System.out.println(formatLine + "added: " + userInput + formatLine);
-                localdb.add(userInput);
+                Task t = new Task(userInput);
+                localdb.add(t);
             }
         }
     }
