973b154b9c2380aa0e415cc7983cff8097019d1c
jun hui <patrickstarjh@gmail.com>
2026-01-23 11:08:13 +0800

    added echos command and exiting

--- src/main/java/Roko.java
+++ src/main/java/Roko.java
@@ -1,7 +1,20 @@
+import java.util.*;
+
 public class Roko {
     public static void main(String[] args) {
         System.out.println("Hello I'm ROKO");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scan = new Scanner(System.in);
+        String formatLine = "\n------------------------------\n";
+        while (true) {
+            String userInput = scan.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(formatLine + "Bye. Hope to see you again!" + formatLine);
+                break;
+            } else {
+                System.out.println(formatLine + userInput + formatLine);
+            }
+        }
     }
 }
